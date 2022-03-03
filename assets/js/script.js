fc11d58346ba3d7cc7531bd06ba35e970843ae8c
JHessler <jhessler11@gmail.com>
2022-03-03 18:59:08 -0500

    fixting button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 const apiKey = "56cd55bcb41fb1d5dd1158c24bb37cc0";
-const PastSearch = [];
+const pastSearch = [];
 
+$(".searchBtn").on("submit", function(event) {
+    
+    event.preventDefault()
 
+    const city = $(".inputCity").val().trim()
+    console.log(city)
+    
+    pastSearch.push(city);
+
+    localStorage.setItem("Cities", city)
+    
+})
 
 
 
 const getCity = city => {
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=city&appid=${apiKey}`, {
         method: "GET",
     })
 
