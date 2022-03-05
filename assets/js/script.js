8220d8288bbc9185e0ca86b9111c4f466ae5bac1
JHessler <jhessler11@gmail.com>
2022-03-05 14:14:26 -0500

    added calls to search city

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,12 @@ $("#aside-container").on("submit", citySearch);
 function citySearch(event) {
     event.preventDefault();
 
-    const city = $("#inputCity").val().trim()
+    const city = $("#inputCity").val()
     $("#inputCity").val("");
     console.log(city)
     
     pastSearch.push(city);
-    localStorage.setItem("Cities", city)
+    
     getCity(city)
     
 }
@@ -32,13 +32,24 @@ function getCity(city) {
     .then((response) => response.json())
     .then((data) => {
         console.log("weather: ", data)
-        $("#humid").append(data.main.humidity);
-        $("#wind").append(data.wind.speed);
-        $("#temp").append(data.main.temp);
-        //displayCityDAta(data);
+        $("#cityName").text(data.name);
+        $("#temp").text(`Temperature: ${data.main.temp} F`);
+        $("#humid").text(`Humidity: ${data.main.humidity}%`);
+        $("#wind").text(`Wind Speed: ${data.wind.speed} MPH`);
+        
+        
+        
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=${apiKey}`)
+        
+        .then((response) => response.json())
+        .then((data) => {
+            $("#index").text(`UV Index: ${data.current.uvi} UV`);
+            
+            
+        })
+        .catch((error) => {});
+        
     }) 
-    .catch((error) => {});
-
 
 
 }
